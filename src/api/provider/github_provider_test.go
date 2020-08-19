76e7f22f8d75294c2a68b95aaf8040e0c17727ee
Jose Pereira <jose.pedro.pereira@criticaltechworks.com>
2020-08-19 18:35:47 +0100

    Implemented Invalid Response Body Test

--- src/api/provider/github_provider_test.go
+++ src/api/provider/github_provider_test.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"errors"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,11 @@ import (
 	"github.com/hetfdex/github-api-go/src/api/model"
 )
 
+func TestMain(m *testing.M) {
+	client.StartMocker()
+
+	os.Exit(m.Run())
+}
 func TestCreateRepoInvalidClientResponse(t *testing.T) {
 	message := "Invalid Client Response"
 
@@ -19,13 +25,39 @@ func TestCreateRepoInvalidClientResponse(t *testing.T) {
 		HTTPMethod: http.MethodPost,
 		Err:        errors.New(message),
 	}
+	client.FlushMocks()
 
-	client.StartMocker()
 	client.AddMock(mock)
 
 	response, err := CreateRepo("", model.GitHubCreateRepoRequest{})
 
 	assert.Nil(t, response)
 	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
 	assert.EqualValues(t, message, err.Message)
+}
+
+func TestCreateRepoInvalidResponseBody(t *testing.T) {
+	invalidResponseBody, _ := os.Open("-asf3")
+
+	response := &http.Response{
+		StatusCode: http.StatusCreated,
+		Body:       invalidResponseBody,
+	}
+
+	mock := client.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Response:   response,
+	}
+	client.FlushMocks()
+
+	client.AddMock(mock)
+
+	res, err := CreateRepo("", model.GitHubCreateRepoRequest{})
+
+	assert.Nil(t, res)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "Invalid Error Response Body", err.Message)
 }
