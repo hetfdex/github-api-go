21b3813ae061205c067a464dbd902ca6902d853b
Jose Pereira <jose.pedro.pereira@criticaltechworks.com>
2020-08-19 18:22:12 +0100

    Implemented Invalid Client Response Test

--- src/api/provider/github_provider_test.go
+++ src/api/provider/github_provider_test.go
@@ -1,11 +1,31 @@
 package provider
 
 import (
+	"errors"
+	"net/http"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
+	"github.com/hetfdex/github-api-go/src/api/client"
 	"github.com/hetfdex/github-api-go/src/api/model"
 )
 
-func TestCreateRepo(t *testing.T) {
-	CreateRepo("", model.GitHubCreateRepoRequest{})
+func TestCreateRepoInvalidClientResponse(t *testing.T) {
+	message := "Invalid Client Response"
+
+	mock := client.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Err:        errors.New(message),
+	}
+
+	client.StartMocker()
+	client.AddMock(mock)
+
+	response, err := CreateRepo("", model.GitHubCreateRepoRequest{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, message, err.Message)
 }
