3c34a04db96b0e9883f199e804db1c347748baa3
Jose Pereira <jose.pedro.pereira@criticaltechworks.com>
2020-08-24 14:42:18 +0100

    Added NewAPIErrorFromBytes Tests

--- src/api/model/api_error_model_test.go
+++ src/api/model/api_error_model_test.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"encoding/json"
 	"net/http"
 	"testing"
 
@@ -8,17 +9,38 @@ import (
 )
 
 const message = "message"
+const statusCode = 424
 
 func TestNewAPIError(t *testing.T) {
-	const statusCode = 424
-
 	error := NewAPIError(statusCode, message)
 
 	assert.NotNil(t, error)
 	assert.EqualValues(t, http.StatusFailedDependency, error.GetStatusCode())
 	assert.EqualValues(t, message, error.GetMessage())
 	assert.EqualValues(t, "", error.GetError())
+}
 
+func TestNewAPIErrorFromBytesInvalidBody(t *testing.T) {
+	var body []byte
+
+	result, err := NewAPIErrorFromBytes(body)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "Invalid JSON Body", err.Error())
+}
+
+func TestNewAPIErrorFromBytes(t *testing.T) {
+	error := NewAPIError(statusCode, message)
+
+	body, _ := json.Marshal(error)
+
+	result, err := NewAPIErrorFromBytes(body)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, statusCode, result.GetStatusCode())
+	assert.EqualValues(t, message, result.GetMessage())
 }
 
 func TestNewNotFoundError(t *testing.T) {
