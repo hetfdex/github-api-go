d323471a85df6f183392841591e38a46d5aabe51
Jose Pereira <jose.pedro.pereira@criticaltechworks.com>
2020-08-24 11:02:42 +0100

    Implemented Repository Service

--- src/api/service/repository_service.go
+++ src/api/service/repository_service.go
@@ -1,9 +1,17 @@
 package service
 
+import (
+	"strings"
+
+	"github.com/hetfdex/github-api-go/src/api/config"
+	"github.com/hetfdex/github-api-go/src/api/model"
+	"github.com/hetfdex/github-api-go/src/api/provider"
+)
+
 type repositoryService struct{}
 
 type repositoryServiceInterface interface {
-	CreateRepo(request interface{}) (interface{}, error)
+	CreateRepo(request model.CreateRepoRequest) (*model.CreateRepoResponse, error)
 }
 
 // RepositoryService entry point
@@ -13,6 +21,26 @@ func init() {
 	var RepositoryService = &repositoryService{}
 }
 
-func (r *repositoryService) CreateRepo(request interface{}) (interface{}, error) {
+func (r *repositoryService) CreateRepo(request model.CreateRepoRequest) (*model.CreateRepoResponse, model.APIError) {
+	request.Name = strings.TrimSpace(request.Name)
 
+	if request.Name == "" {
+		return nil, model.NewBadRequestError("Invalid Repository Name")
+	}
+	req := model.GitHubCreateRepoRequest{
+		Name:        request.Name,
+		Description: request.Description,
+		Private:     false,
+	}
+	res, err := provider.CreateRepo(config.GetGitHubAccessToken(), req)
+
+	if err != nil {
+		return nil, model.NewAPIError(err.StatusCode, err.Message)
+	}
+	response := &model.CreateRepoResponse{
+		ID:    res.ID,
+		Name:  res.Name,
+		Owner: res.Owner.Login,
+	}
+	return response, nil
 }
