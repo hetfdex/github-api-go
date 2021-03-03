3c78096547526250463863e558816d36ad28b719
José Pereira <hetfdex@gmail.com>
2021-03-03 12:04:11 +0000

    Removed old code

--- src/api/app/url_mappings.go
+++
@@ -1,8 +0,0 @@
-package app
-
-import "github.com/hetfdex/github-api-go/src/api/controller"
-
-func mapURLs() {
-	router.GET("/health-check", controller.CheckHealth)
-	router.POST("/repositories", controller.CreateRepo)
-}
