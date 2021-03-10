53621a2a247a267f8d5ac479283dd998145f5b89
José Pereira <hetfdex@gmail.com>
2021-03-10 11:54:56 +0000

    Created GitHub Models

---
+++ model/github/github_create_repo_response_model.go
@@ -0,0 +1,22 @@
+package github
+
+type CreateRepoResponse struct {
+	ID          int         `json:"id"`
+	Name        string      `json:"name"`
+	FullName    string      `json:"full_name"`
+	Owner       Owner       `json:"owner"`
+	Permissions Permissions `json:"permissions"`
+}
+
+type Owner struct {
+	Login   string `json:"login"`
+	ID      int    `json:"id"`
+	URL     string `json:"url"`
+	HtmlUrl string `json:"html_url"`
+}
+
+type Permissions struct {
+	Admin bool `json:"admin"`
+	Push  bool `json:"push"`
+	Pull  bool `json:"pull"`
+}
