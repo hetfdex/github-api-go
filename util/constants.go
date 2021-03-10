08a099250db201771fb63bb4953c6dac1b239d97
José Pereira <hetfdex@gmail.com>
2021-03-10 11:54:41 +0000

    Created Constants

---
+++ util/constants.go
@@ -0,0 +1,9 @@
+package util
+
+const AuthorizationHeaderKey = "Authorization"
+
+const AuthorizationHeaderValue = "token %s"
+
+const UnmarshalJsonFailureMessage = "Failed to parse json into model"
+
+const CreateRepoUrl = "https://api.github.com/user/repos"
