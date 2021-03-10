d88e93aea15b95e7b2e707cde4d045953b48ddd1
José Pereira <hetfdex@gmail.com>
2021-03-10 15:45:02 +0000

    Added mocking options in REST Client

--- client/rest/rest_client.go
+++ client/rest/rest_client.go
@@ -2,10 +2,42 @@ package rest
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
+var mocking = false
+
+var mocks map[string]*Mock
+
+type Mock struct {
+	Url      string
+	Method   string
+	Response *http.Response
+	Err      error
+}
+
+func StartMock(mock *Mock) {
+	mocks = make(map[string]*Mock)
+
+	id := getMockId(mock.Method, mock.Url)
+
+	mocks[id] = mock
+
+	mocking = true
+}
+
+func StopMock() {
+	mocking = false
+
+	mocks = make(map[string]*Mock)
+}
+
 func Post(url string, header http.Header, body *bytes.Reader) (*http.Response, error) {
+	if mocking {
+		return postMock(url)
+	}
 	client := http.Client{}
 
 	req, err := http.NewRequest(http.MethodPost, url, body)
@@ -23,4 +55,23 @@ func Post(url string, header http.Header, body *bytes.Reader) (*http.Response, e
 
 	}
 	return res, nil
+}
+
+func postMock(url string) (*http.Response, error) {
+	id := getMockId(http.MethodPost, url)
+
+	mock := mocks[id]
+
+	if mock == nil {
+		message := fmt.Sprintf("no mock available for %v", url)
+
+		err := errors.New(message)
+
+		return nil, err
+	}
+	return mock.Response, mock.Err
+}
+
+func getMockId(method string, url string) string {
+	return fmt.Sprintf("%s_%s", method, url)
 }
