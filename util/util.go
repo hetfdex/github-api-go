01ba7533cf3f438584a70e3a0314a85fefaf97c6
José Pereira <hetfdex@gmail.com>
2021-03-10 11:55:11 +0000

    Implemented Utils

---
+++ util/util.go
@@ -0,0 +1,50 @@
+package util
+
+import (
+	"encoding/json"
+	"github.com/hetfdex/github-api-go/model/github"
+	"net/http"
+)
+
+func NewNotFoundError(message string) *github.ErrorResponse {
+	return newError(http.StatusNotFound, message)
+}
+
+func NewBadRequestError(message string) *github.ErrorResponse {
+	return newError(http.StatusBadRequest, message)
+}
+
+func NewInternalServerError(message string) *github.ErrorResponse {
+	return newError(http.StatusInternalServerError, message)
+}
+
+func newError(statusCode int, message string) *github.ErrorResponse {
+	return &github.ErrorResponse{
+		StatusCode: statusCode,
+		Message:    message,
+	}
+}
+
+func NewErrorFromBytes(statusCode int, body []byte) (*github.ErrorResponse, *github.ErrorResponse) {
+	var result github.ErrorResponse
+
+	err := json.Unmarshal(body, &result)
+
+	if err != nil {
+		return nil, NewInternalServerError(UnmarshalJsonFailureMessage)
+	}
+	result.StatusCode = statusCode
+
+	return &result, nil
+}
+
+func NewCreateRepoFromBytes(body []byte) (*github.CreateRepoResponse, *github.ErrorResponse) {
+	var result github.CreateRepoResponse
+
+	err := json.Unmarshal(body, &result)
+
+	if err != nil {
+		return nil, NewInternalServerError(UnmarshalJsonFailureMessage)
+	}
+	return &result, nil
+}
