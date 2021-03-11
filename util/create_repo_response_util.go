8803183e5ad457ad04a3efc68fadc632bc308ea9
José Pereira <hetfdex@gmail.com>
2021-03-11 16:10:28 +0000

    Fixed code

--- util/create_repo_response_util.go
+++ util/create_repo_response_util.go
@@ -2,17 +2,16 @@ package util
 
 import (
 	"encoding/json"
-	"github.com/hetfdex/github-api-go/dto"
 	"github.com/hetfdex/github-api-go/model"
 )
 
-func NewCreateRepoResponseFromBytes(body []byte) (*model.CreateRepoResponse, *dto.ErrorResponse) {
+func NewCreateRepoResponseFromBytes(body []byte) (*model.CreateRepoResponse, *model.ErrorResponse) {
 	var result model.CreateRepoResponse
 
 	err := json.Unmarshal(body, &result)
 
 	if err != nil {
-		return nil, NewInternalServerDtoError(err.Error())
+		return nil, NewInternalServerError(err.Error())
 	}
 	return &result, nil
 }
