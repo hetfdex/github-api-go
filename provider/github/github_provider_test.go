4d0ebf63740d1e1975ccbe02aec27b4fdce5a8a1
José Pereira <hetfdex@gmail.com>
2021-03-11 11:18:32 +0000

    Refactored tests for Provider

--- provider/github/github_provider_test.go
+++ provider/github/github_provider_test.go
@@ -1,57 +1,36 @@
 package github
 
 import (
-	"bytes"
-	"errors"
-	"github.com/hetfdex/github-api-go/client/rest"
+	"github.com/hetfdex/github-api-go/mock"
 	"github.com/hetfdex/github-api-go/model/github"
-	"github.com/hetfdex/github-api-go/util"
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
 	"net/http"
-	"os"
-	"strings"
 	"testing"
 )
 
 func TestCreateRepoPostError(t *testing.T) {
-	mock := &rest.Mock{
-		Url:    util.CreateRepoUrl,
-		Method: http.MethodPost,
-		Err:    errors.New("invalid response"),
-	}
-	rest.StartMock(mock)
-
 	var req github.CreateRepoRequest
 
-	res, err := Provider.CreateRepo(req, "")
+	Provider = &provider{
+		&mock.PosterMock{},
+	}
 
-	rest.StopMock()
+	res, err := Provider.CreateRepo(req, mock.PostErrorAuthorizationHeaderValue)
 
 	assert.Nil(t, res)
 	assert.NotNil(t, err)
 	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
-	assert.EqualValues(t, mock.Err.Error(), err.Message)
+	assert.EqualValues(t, mock.PostErrorAuthorizationHeaderValue, err.Message)
 }
 
 func TestCreateRepoHandleResponseReadAllError(t *testing.T) {
-	body, _ := os.Open("-asf3")
-
-	mock := &rest.Mock{
-		Url:    util.CreateRepoUrl,
-		Method: http.MethodPost,
-		Response: &http.Response{
-			StatusCode: http.StatusFailedDependency,
-			Body:       body,
-		},
-	}
-	rest.StartMock(mock)
-
 	var req github.CreateRepoRequest
 
-	res, err := Provider.CreateRepo(req, "")
+	Provider = &provider{
+		&mock.PosterMock{},
+	}
 
-	rest.StopMock()
+	res, err := Provider.CreateRepo(req, mock.HandleResponseReadAllErrorAuthorizationHeaderValue)
 
 	assert.Nil(t, res)
 	assert.NotNil(t, err)
@@ -60,49 +39,28 @@ func TestCreateRepoHandleResponseReadAllError(t *testing.T) {
 }
 
 func TestCreateRepoHandleResponseNotOkNewErrorFromBytesError(t *testing.T) {
-	mock := &rest.Mock{
-		Url:    util.CreateRepoUrl,
-		Method: http.MethodPost,
-		Response: &http.Response{
-			StatusCode: http.StatusFailedDependency,
-			Body:       ioutil.NopCloser(bytes.NewBufferString("bla")),
-		},
-	}
-	rest.StartMock(mock)
-
 	var req github.CreateRepoRequest
 
-	res, err := Provider.CreateRepo(req, "")
+	Provider = &provider{
+		&mock.PosterMock{},
+	}
 
-	rest.StopMock()
+	res, err := Provider.CreateRepo(req, mock.HandleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue)
 
 	assert.Nil(t, res)
 	assert.NotNil(t, err)
 	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
-	assert.EqualValues(t, util.UnmarshalJsonFailureMessage, err.Message)
+	assert.EqualValues(t, "json: cannot unmarshal number into Go struct field ErrorResponse.message of type string", err.Message)
 }
 
 func TestCreateRepoHandleResponseNotOk(t *testing.T) {
-	body := ioutil.NopCloser(strings.NewReader(`{
-		"message": "message",
-		"documentation_url": "documentation_url"
-		}`))
-
-	mock := &rest.Mock{
-		Url:    util.CreateRepoUrl,
-		Method: http.MethodPost,
-		Response: &http.Response{
-			StatusCode: http.StatusFailedDependency,
-			Body:       body,
-		},
-	}
-	rest.StartMock(mock)
-
 	var req github.CreateRepoRequest
 
-	res, err := Provider.CreateRepo(req, "")
+	Provider = &provider{
+		&mock.PosterMock{},
+	}
 
-	rest.StopMock()
+	res, err := Provider.CreateRepo(req, mock.HandleResponseNotOkAuthorizationHeaderValue)
 
 	assert.Nil(t, res)
 	assert.NotNil(t, err)
@@ -112,61 +70,28 @@ func TestCreateRepoHandleResponseNotOk(t *testing.T) {
 }
 
 func TestCreateRepoHandleResponseOkNewCreateRepoResponseFromBytesError(t *testing.T) {
-	mock := &rest.Mock{
-		Url:    util.CreateRepoUrl,
-		Method: http.MethodPost,
-		Response: &http.Response{
-			StatusCode: http.StatusCreated,
-			Body:       ioutil.NopCloser(bytes.NewBufferString("bla")),
-		},
-	}
-	rest.StartMock(mock)
-
 	var req github.CreateRepoRequest
 
-	res, err := Provider.CreateRepo(req, "")
+	Provider = &provider{
+		&mock.PosterMock{},
+	}
 
-	rest.StopMock()
+	res, err := Provider.CreateRepo(req, mock.HandleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue)
 
 	assert.Nil(t, res)
 	assert.NotNil(t, err)
 	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
-	assert.EqualValues(t, util.UnmarshalJsonFailureMessage, err.Message)
+	assert.EqualValues(t, "invalid character '\"' after object key:value pair", err.Message)
 }
 
 func TestCreateRepoHandleResponseOk(t *testing.T) {
-	body := ioutil.NopCloser(strings.NewReader(`{
-		"id": 0,
-		"name": "name",
-		"full_name": "full_name",
-		"owner": {
-			"login": "login",
-			"id": 0,
-			"url": "url",
-			"html_url": "html_url"
-			},
-		"permissions": {
-			"admin": true,
-			"push": true,
-			"pull": true
-			}
-		}`))
-
-	mock := &rest.Mock{
-		Url:    util.CreateRepoUrl,
-		Method: http.MethodPost,
-		Response: &http.Response{
-			StatusCode: http.StatusCreated,
-			Body:       body,
-		},
-	}
-	rest.StartMock(mock)
-
 	var req github.CreateRepoRequest
 
-	res, err := Provider.CreateRepo(req, "")
+	Provider = &provider{
+		&mock.PosterMock{},
+	}
 
-	rest.StopMock()
+	res, err := Provider.CreateRepo(req, "")
 
 	assert.Nil(t, err)
 	assert.NotNil(t, res)
