7d26f2e5760cd3b255d6cdf0150e264a6b9fee48
José Pereira <hetfdex@gmail.com>
2021-03-11 09:05:44 +0000

    Implemented tests for Client

---
+++ client/client_test.go
@@ -0,0 +1,48 @@
+package client
+
+import (
+	"bytes"
+	"github.com/hetfdex/github-api-go/mock"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+var reader = bytes.NewReader([]byte(`test`))
+
+var header = http.Header{}
+
+func TestPostNewRequestError(t *testing.T) {
+	url := ":abc1{DEf2=test@test.com:666/db?"
+
+	Client = &mock.DoerMock{}
+
+	res, err := Post(url, header, reader)
+
+	assert.Nil(t, res)
+	assert.NotNil(t, err)
+}
+
+func TestPostError(t *testing.T) {
+	url := mock.DoErrorUrl
+
+	Client = &mock.DoerMock{}
+
+	res, err := Post(url, header, reader)
+
+	assert.Nil(t, res)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, mock.DoErrorUrl, err.Error())
+}
+
+func TestPostOk(t *testing.T) {
+	url := "https://www.google.com"
+
+	Client = &mock.DoerMock{}
+
+	res, err := Post(url, header, reader)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	assert.EqualValues(t, http.StatusOK, res.StatusCode)
+}
