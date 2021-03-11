20fdcbc06d9cfa2820e4d1c535796356e5572dec
José Pereira <hetfdex@gmail.com>
2021-03-11 15:45:26 +0000

    Fixed NewCreateRepoResponseFromBytes (wip)

--- util/create_repo_response_util.go
+++ util/create_repo_response_util.go
@@ -2,16 +2,17 @@ package util
 
 import (
 	"encoding/json"
+	"github.com/hetfdex/github-api-go/dto"
 	"github.com/hetfdex/github-api-go/model"
 )
 
-func NewCreateRepoResponseFromBytes(body []byte) (*model.CreateRepoResponse, *model.ErrorResponse) {
+func NewCreateRepoResponseFromBytes(body []byte) (*model.CreateRepoResponse, *dto.ErrorResponse) {
 	var result model.CreateRepoResponse
 
 	err := json.Unmarshal(body, &result)
 
 	if err != nil {
-		return nil, NewInternalServerError(err.Error())
+		return nil, NewInternalServerDtoError(err.Error())
 	}
 	return &result, nil
 }
