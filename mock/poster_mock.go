c3aa3182f825b0a8fc36a5738f3d426462cdecc9
José Pereira <hetfdex@gmail.com>
2021-03-11 12:18:17 +0000

    Refactored PosterMock

--- mock/poster_mock.go
+++ mock/poster_mock.go
@@ -22,52 +22,75 @@ type PosterMock struct {
 }
 
 func (*PosterMock) Post(_ string, header http.Header, _ *bytes.Reader) (*http.Response, error) {
-	authorizationHeaderValue := header.Get(util.AuthorizationHeaderKey)
-
 	postErrorAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, PostErrorAuthorizationHeaderValue)
 	readAllErrorAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, HandleResponseReadAllErrorAuthorizationHeaderValue)
 	handleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, HandleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue)
 	handleResponseNotOkAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, HandleResponseNotOkAuthorizationHeaderValue)
 	handleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, HandleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue)
 
-	if authorizationHeaderValue == postErrorAuthorizationHeaderValue {
-		return nil, errors.New(PostErrorAuthorizationHeaderValue)
-	} else if authorizationHeaderValue == readAllErrorAuthorizationHeaderValue {
-		body, _ := os.Open("-asf3")
+	switch header.Get(util.AuthorizationHeaderKey) {
+	case postErrorAuthorizationHeaderValue:
+		return postErrorResponse()
+	case readAllErrorAuthorizationHeaderValue:
+		return readAllErrorResponse()
+	case handleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue:
+		return handleResponseNotOkNewErrorFromBytesError()
+	case handleResponseNotOkAuthorizationHeaderValue:
+		return handleResponseNotOk()
+	case handleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue:
+		return handleResponseOkNewCreateRepoResponseFromBytesError()
+	}
+	return handleResponseOk()
+}
 
-		return &http.Response{
-			StatusCode: http.StatusFailedDependency,
-			Body:       body,
-		}, nil
-	} else if authorizationHeaderValue == handleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue {
-		body := ioutil.NopCloser(strings.NewReader(`{
+func postErrorResponse() (*http.Response, error) {
+	return nil, errors.New(PostErrorAuthorizationHeaderValue)
+}
+
+func readAllErrorResponse() (*http.Response, error) {
+	body, _ := os.Open("-asf3")
+
+	return &http.Response{
+		StatusCode: http.StatusFailedDependency,
+		Body:       body,
+	}, nil
+}
+
+func handleResponseNotOkNewErrorFromBytesError() (*http.Response, error) {
+	body := ioutil.NopCloser(strings.NewReader(`{
 		"message": 0
 		}`))
 
-		return &http.Response{
-			StatusCode: http.StatusFailedDependency,
-			Body:       body,
-		}, nil
-	} else if authorizationHeaderValue == handleResponseNotOkAuthorizationHeaderValue {
-		body := ioutil.NopCloser(strings.NewReader(`{
+	return &http.Response{
+		StatusCode: http.StatusFailedDependency,
+		Body:       body,
+	}, nil
+}
+
+func handleResponseNotOk() (*http.Response, error) {
+	body := ioutil.NopCloser(strings.NewReader(`{
 		"message": "message",
 		"documentation_url": "documentation_url"
 		}`))
 
-		return &http.Response{
-			StatusCode: http.StatusFailedDependency,
-			Body:       body,
-		}, nil
-	} else if authorizationHeaderValue == handleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue {
-		body := ioutil.NopCloser(strings.NewReader(`{
+	return &http.Response{
+		StatusCode: http.StatusFailedDependency,
+		Body:       body,
+	}, nil
+}
+
+func handleResponseOkNewCreateRepoResponseFromBytesError() (*http.Response, error) {
+	body := ioutil.NopCloser(strings.NewReader(`{
 		"id": 0"
 		}`))
 
-		return &http.Response{
-			StatusCode: http.StatusCreated,
-			Body:       body,
-		}, nil
-	}
+	return &http.Response{
+		StatusCode: http.StatusCreated,
+		Body:       body,
+	}, nil
+}
+
+func handleResponseOk() (*http.Response, error) {
 	body := ioutil.NopCloser(strings.NewReader(`{
 		"id": 0,
 		"name": "name",
