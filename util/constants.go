172578433b1718e3162ffe19e73b95f08001b777
José Pereira <hetfdex@gmail.com>
2021-03-11 16:09:51 +0000

    Added Constant

--- util/constants.go
+++ util/constants.go
@@ -5,3 +5,5 @@ const AuthorizationHeaderKey = "Authorization"
 const AuthorizationHeaderValue = "token %s"
 
 const CreateRepoUrl = "https://api.github.com/user/repos"
+
+const InvalidRepoNameError = "invalid repository name"
