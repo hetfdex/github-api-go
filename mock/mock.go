6776553a7d0dd92ca5ce5dc57872d43c6da604f7
José Pereira <hetfdex@gmail.com>
2021-03-11 11:17:54 +0000

    Added mock for Post

--- mock/mock.go
+++ mock/mock.go
@@ -1,21 +1,106 @@
 package mock
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
+	"github.com/hetfdex/github-api-go/util"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 )
 
+const DoErrorUrl = "doErrorUrl"
+const PostErrorAuthorizationHeaderValue = "postError"
+const HandleResponseReadAllErrorAuthorizationHeaderValue = "handleResponseReadAllError"
+const HandleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue = "handleResponseNotOkNewErrorFromBytesError"
+const HandleResponseNotOkAuthorizationHeaderValue = "handleResponseNotOk"
+const HandleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue = "handleResponseOkNewCreateRepoResponseFromBytesError"
+
 type DoerMock struct {
-	DoFunc func(req *http.Request) (*http.Response, error)
+	DoFunc func(*http.Request) (*http.Response, error)
 }
 
-const DoErrorUrl = "doErrorUrl"
+type PosterMock struct {
+	PostFunc func(string, http.Header, *bytes.Reader) (*http.Response, error)
+}
 
-func (m *DoerMock) Do(req *http.Request) (*http.Response, error) {
+func (*DoerMock) Do(req *http.Request) (*http.Response, error) {
 	if req.URL.String() == DoErrorUrl {
 		return nil, errors.New(DoErrorUrl)
 	}
 	return &http.Response{
-		StatusCode: 200,
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func (*PosterMock) Post(_ string, header http.Header, _ *bytes.Reader) (*http.Response, error) {
+	authorizationHeaderValue := header.Get(util.AuthorizationHeaderKey)
+
+	postErrorAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, PostErrorAuthorizationHeaderValue)
+	readAllErrorAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, HandleResponseReadAllErrorAuthorizationHeaderValue)
+	handleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, HandleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue)
+	handleResponseNotOkAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, HandleResponseNotOkAuthorizationHeaderValue)
+	handleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue := fmt.Sprintf(util.AuthorizationHeaderValue, HandleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue)
+
+	if authorizationHeaderValue == postErrorAuthorizationHeaderValue {
+		return nil, errors.New(PostErrorAuthorizationHeaderValue)
+	} else if authorizationHeaderValue == readAllErrorAuthorizationHeaderValue {
+		body, _ := os.Open("-asf3")
+
+		return &http.Response{
+			StatusCode: http.StatusFailedDependency,
+			Body:       body,
+		}, nil
+	} else if authorizationHeaderValue == handleResponseNotOkNewErrorFromBytesErrorAuthorizationHeaderValue {
+		body := ioutil.NopCloser(strings.NewReader(`{
+		"message": 0
+		}`))
+
+		return &http.Response{
+			StatusCode: http.StatusFailedDependency,
+			Body:       body,
+		}, nil
+	} else if authorizationHeaderValue == handleResponseNotOkAuthorizationHeaderValue {
+		body := ioutil.NopCloser(strings.NewReader(`{
+		"message": "message",
+		"documentation_url": "documentation_url"
+		}`))
+
+		return &http.Response{
+			StatusCode: http.StatusFailedDependency,
+			Body:       body,
+		}, nil
+	} else if authorizationHeaderValue == handleResponseOkNewCreateRepoResponseFromBytesErrorAuthorizationHeaderValue {
+		body := ioutil.NopCloser(strings.NewReader(`{
+		"id": 0"
+		}`))
+
+		return &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       body,
+		}, nil
+	}
+	body := ioutil.NopCloser(strings.NewReader(`{
+		"id": 0,
+		"name": "name",
+		"full_name": "full_name",
+		"owner": {
+			"login": "login",
+			"id": 0,
+			"url": "url",
+			"html_url": "html_url"
+			},
+		"permissions": {
+			"admin": true,
+			"push": true,
+			"pull": true
+			}
+		}`))
+
+	return &http.Response{
+		StatusCode: http.StatusCreated,
+		Body:       body,
 	}, nil
 }
