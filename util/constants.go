55a291da14725d8b60153e50f6e64cad77f26028
José Pereira <hetfdex@gmail.com>
2021-03-12 15:43:42 +0000

    Added constants

--- util/constants.go
+++ util/constants.go
@@ -1,12 +1,16 @@
 package util
 
+const HealthCheckPath = "/health-check"
+
+const RepositoriesPath = "/repositories"
+
+const CreateRepoUrl = "https://api.github.com/user/repos"
+
 const GitHubTokenKey = "SECRET_GITHUB_TOKEN"
 
 const AuthorizationHeaderKey = "Authorization"
 
 const AuthorizationHeaderValue = "token %s"
-
-const CreateRepoUrl = "https://api.github.com/user/repos"
 
 const InvalidRepoNameError = "invalid repository name"
 
