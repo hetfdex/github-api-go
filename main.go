89e0ab17730a6ccdc86034e364fdae2e754ceacd
José Pereira <hetfdex@gmail.com>
2021-03-12 15:44:41 +0000

    Fixed main

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/hetfdex/github-api-go/app"
+import (
+	"github.com/hetfdex/github-api-go/router"
+)
 
 func main() {
-	app.App.Start()
+	rtr := router.Router.Setup()
+
+	err := rtr.Run("localhost:8080")
+
+	if err != nil {
+		panic(err)
+	}
 }
