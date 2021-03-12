7675fe0ab8fea529c7789b8eba765577fd9489f9
José Pereira <hetfdex@gmail.com>
2021-03-12 12:47:50 +0000

    Defined mocks

--- mock/provider_repo_creator_mock.go
+++ mock/provider_repo_creator_mock.go
@@ -4,10 +4,30 @@ import (
 	"github.com/hetfdex/github-api-go/model"
 )
 
+const ServiceCreateRepoError = "serviceCreateRepoError"
+
 type ProviderRepoCreatorMock struct {
 	CreateRepoFunc func(model.CreateRepoRequest, string) (*model.CreateRepoResponse, *model.ErrorResponse)
 }
 
-func (*ProviderRepoCreatorMock) CreateRepo(model.CreateRepoRequest, string) (*model.CreateRepoResponse, *model.ErrorResponse) {
-	return nil, nil
+func (*ProviderRepoCreatorMock) CreateRepo(req model.CreateRepoRequest, _ string) (*model.CreateRepoResponse, *model.ErrorResponse) {
+	if req.Name == ServiceCreateRepoError {
+		return nil, model.NewInternalServerError(ServiceCreateRepoError)
+	}
+	return &model.CreateRepoResponse{
+		ID:       0,
+		Name:     "name",
+		FullName: "fullName",
+		Owner: model.Owner{
+			Login:   "owner_login",
+			ID:      0,
+			URL:     "url",
+			HtmlUrl: "htmlUrl",
+		},
+		Permissions: model.Permissions{
+			Admin: false,
+			Push:  false,
+			Pull:  false,
+		},
+	}, nil
 }
--- mock/service_repo_creator_mock.go
+++ mock/service_repo_creator_mock.go
@@ -4,10 +4,19 @@ import (
 	"github.com/hetfdex/github-api-go/model"
 )
 
+const ControllerCreateRepoError = "controllerCreateRepoError"
+
 type ServiceRepoCreatorMock struct {
 	CreateRepoFunc func(model.CreateRepoRequestDto) (*model.CreateRepoResponseDto, *model.ErrorResponseDto)
 }
 
-func (*ServiceRepoCreatorMock) CreateRepo(model.CreateRepoRequestDto) (*model.CreateRepoResponseDto, *model.ErrorResponseDto) {
-	return nil, nil
+func (*ServiceRepoCreatorMock) CreateRepo(reqDto model.CreateRepoRequestDto) (*model.CreateRepoResponseDto, *model.ErrorResponseDto) {
+	if reqDto.Name == ControllerCreateRepoError {
+		return nil, model.NewInternalServerDtoError(ControllerCreateRepoError)
+	}
+	return &model.CreateRepoResponseDto{
+		ID:    0,
+		Name:  "name",
+		Owner: "owner",
+	}, nil
 }
