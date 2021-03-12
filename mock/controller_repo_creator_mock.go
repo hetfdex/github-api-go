444999a21fba10396cd72a5fa20bbe104e2049b6
José Pereira <hetfdex@gmail.com>
2021-03-12 14:55:25 +0000

    Implemented mocks

--- mock/controller_repo_creator_mock.go
+++ mock/controller_repo_creator_mock.go
@@ -2,12 +2,17 @@ package mock
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+const createRepoCreated = "createRepoCreated"
 
 type ControllerRepoCreatorMock struct {
 	CreateRepoFunc func(ctx *gin.Context)
 }
 
 func (*ControllerRepoCreatorMock) CreateRepo(ctx *gin.Context) {
+	ctx.JSON(http.StatusCreated, createRepoCreated)
+
 	return
 }
--- mock/health_checker_mock.go
+++ mock/health_checker_mock.go
@@ -2,12 +2,17 @@ package mock
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+const checkHealthOk = "checkHealthOk"
 
 type HealthCheckerMock struct {
 	CheckHealthFunc func(ctx *gin.Context)
 }
 
-func (*HealthCheckerMock) CheckHealth(*gin.Context) {
+func (*HealthCheckerMock) CheckHealth(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, checkHealthOk)
+
 	return
 }
