29f6f476b92b75d40bd9d7a8e08f4dcfffc0b486
José Pereira <hetfdex@gmail.com>
2021-03-15 10:42:18 +0000

    Added tests

--- service/service_test.go
+++ service/service_test.go
@@ -11,6 +11,8 @@ import (
 )
 
 var reqDto model.CreateRepoRequestDto
+var reqDtoConcurrent model.CreateRepoRequestDto
+var reqsDto model.CreateReposRequestDto
 
 func TestMain(m *testing.M) {
 	Service = &service{
@@ -59,4 +61,82 @@ func TestCreateRepo(t *testing.T) {
 	assert.NotNil(t, res)
 	assert.EqualValues(t, 0, res.ID)
 	assert.EqualValues(t, reqDto.Name, res.Name)
+}
+
+func TestCreateReposError(t *testing.T) {
+	reqDto = model.CreateRepoRequestDto{
+		Name:        mock.ServiceCreateRepoError,
+		Description: "description",
+	}
+
+	reqDtoConcurrent = model.CreateRepoRequestDto{
+		Name:        mock.ServiceCreateRepoError,
+		Description: "description",
+	}
+
+	reqsDto = model.CreateReposRequestDto{
+		Requests: []model.CreateRepoRequestDto{
+			reqDto,
+			reqDtoConcurrent,
+		},
+	}
+
+	res := Service.CreateRepos(reqsDto)
+
+	assert.NotNil(t, res)
+	assert.NotNil(t, res.Responses)
+	assert.NotNil(t, res.Errors)
+	assert.EqualValues(t, http.StatusInternalServerError, res.StatusCode)
+}
+
+func TestCreateReposMixed(t *testing.T) {
+	reqDto = model.CreateRepoRequestDto{
+		Name:        "name",
+		Description: "description",
+	}
+
+	reqDtoConcurrent = model.CreateRepoRequestDto{
+		Name:        mock.ServiceCreateRepoError,
+		Description: "description",
+	}
+
+	reqsDto = model.CreateReposRequestDto{
+		Requests: []model.CreateRepoRequestDto{
+			reqDto,
+			reqDtoConcurrent,
+		},
+	}
+
+	res := Service.CreateRepos(reqsDto)
+
+	assert.NotNil(t, res)
+	assert.NotNil(t, res.Responses)
+	assert.NotNil(t, res.Errors)
+	assert.EqualValues(t, http.StatusPartialContent, res.StatusCode)
+}
+
+func TestCreateRepos(t *testing.T) {
+	reqDto = model.CreateRepoRequestDto{
+		Name:        "name",
+		Description: "description",
+	}
+
+	reqDtoConcurrent = model.CreateRepoRequestDto{
+		Name:        "name",
+		Description: "description",
+	}
+
+	reqsDto = model.CreateReposRequestDto{
+		Requests: []model.CreateRepoRequestDto{
+			reqDto,
+			reqDtoConcurrent,
+		},
+	}
+
+	res := Service.CreateRepos(reqsDto)
+
+	assert.NotNil(t, res)
+	assert.NotNil(t, res.Responses)
+	assert.NotNil(t, res.Errors)
+	assert.EqualValues(t, http.StatusCreated, res.StatusCode)
 }
